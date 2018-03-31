a4b787034eaa5bc2057766029993f77895684e3a
Soumyajit Dutta <sanudatta11@gmail.com>
2018-03-31 10:00:52 -0600

    Added For Loop

--- project.c
+++ project.c
@@ -20,4 +20,26 @@ int main()
 		scanf("%d",&nProcess);
 	}
 	process pros[nProcess];
+
+	for(int i=0; i<nProcess; ++i){
+		printf("Enter the input for Process %d\n", i+1 );
+		pros[i].id = i+1;
+		pros[i].priority = 1;
+		pros[i].waitTime = 0;
+		pros[i].arrivalTime = 0;
+		while(pros[i].arrivalTime > -1){
+			printf("Enter Arrival Time=");
+			scanf("%d",&pros[i].arrivalTime);
+			if(pros[i].arrivalTime > -1)
+				break;
+		}
+		pros[i].burstTime = 0;
+		while(pros[i].burstTime > -1){	
+			printf("Enter Burst Time=");
+			scanf("%d",&pros[i].burstTime);
+			if(pros[i].burstTime > -1)
+				break;
+		}
+		printf("\n");
+	}
 }
