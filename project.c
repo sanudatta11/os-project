1de7ec5c29ee241abf12a78d2b45d2aa0d50aad2
Soumyajit Dutta <sanudatta11@users.noreply.github.com>
2018-04-09 22:13:27 +0530

    Update project.c

--- project.c
+++ project.c
@@ -11,48 +11,6 @@ struct process{
 	double priority ;
 };
 
-void sortOnArrival(struct process proc[]);
-void startProcessing(struct process proc[]);
-void updateProcesses(struct process proc[], int pTime, int currentProcess);
-void printProcess(struct process proc[]);
-
-int main()
-{
-	printf("Enter the no. of Process\n");
-	scanf("%d",&nProcess);
-	while(nProcess < 1){
-		printf("Enter Process Count greater than one.\n");
-		scanf("%d",&nProcess);
-	}
-	struct process proc[nProcess];
-
-	for(int i=0; i<nProcess; ++i){
-		printf("Enter the input for Process %d\n", i+1 );
-		proc[i].id = i+1;
-		proc[i].priority = 1;
-		proc[i].waitTime = 0;
-		proc[i].arrivalTime = 0;
-		while(proc[i].arrivalTime > -1){
-			printf("Enter Arrival Time=");
-			scanf("%d",&proc[i].arrivalTime);
-			if(proc[i].arrivalTime > -1)
-				break;
-		}
-		proc[i].burstTime = 0;
-		while(proc[i].burstTime > -1){	
-			printf("Enter Burst Time=");
-			scanf("%d",&proc[i].burstTime);
-			if(proc[i].burstTime > -1)
-				break;
-		}
-		printf("\n");
-	}
-	sortOnArrival(proc);
-	startProcessing(proc);
-	printProcess(proc);
-	return 0;
-}
-
 void sortOnArrival(struct process proc[]){
 	for(int i=0; i<nProcess-1; ++i){
 		for(int j=i+1; j<nProcess; ++j){
@@ -93,4 +51,41 @@ void printProcess(struct process proc[]){
 	for(int i=0; i<nProcess; i++){
 		printf("The Proccess :%d arivlTime : %d  burstTime : %d\n\n", proc[i].id, proc[i].arrivalTime, proc[i].burstTime);
 	}
+}
+
+int main()
+{
+	printf("Enter the no. of Process\n");
+	scanf("%d",&nProcess);
+	while(nProcess < 1){
+		printf("Enter Process Count greater than one.\n");
+		scanf("%d",&nProcess);
+	}
+	struct process proc[nProcess];
+
+	for(int i=0; i<nProcess; ++i){
+		printf("Enter the input for Process %d\n", i+1 );
+		proc[i].id = i+1;
+		proc[i].priority = 1;
+		proc[i].waitTime = 0;
+		proc[i].arrivalTime = 0;
+		while(proc[i].arrivalTime > -1){
+			printf("Enter Arrival Time=");
+			scanf("%d",&proc[i].arrivalTime);
+			if(proc[i].arrivalTime > -1)
+				break;
+		}
+		proc[i].burstTime = 0;
+		while(proc[i].burstTime > -1){	
+			printf("Enter Burst Time=");
+			scanf("%d",&proc[i].burstTime);
+			if(proc[i].burstTime > -1)
+				break;
+		}
+		printf("\n");
+	}
+	sortOnArrival(proc);
+	startProcessing(proc);
+	printProcess(proc);
+	return 0;
 }
