4b04203fe2ba762d7b72a2a406b6a84efe4f26bf
Soumyajit Dutta <sanudatta11@gmail.com>
2018-04-10 00:03:29 +0530

    Final touchup

--- project.c
+++ project.c
@@ -10,6 +10,21 @@ struct process{
 	int waitTime ;
 	double priority ;
 };
+void updateProcesses(struct process proc[], int pTime, int currentProcess){
+	for(int i = currentProcess; i<nProcess; i++){
+		float waitTime = pTime - proc[i].arrivalTime;
+		proc[i].priority = 1  + waitTime/(float)proc[i].burstTime;
+	}
+	for(int i=currentProcess; i<nProcess-1; i++){
+		for(int j=i+1; j<nProcess; j++){
+			if(proc[i].priority < proc[j].priority){
+				struct process a = proc[j];
+				proc[j] = proc[i];
+				proc[i] = a;
+			}
+		}
+	}
+}
 
 void sortOnArrival(struct process proc[]){
 	for(int i=0; i<nProcess-1; ++i){
@@ -31,34 +46,20 @@ void startProcessing(struct process proc[]){
 		updateProcesses(proc,pTime,currentProcess);
 	}
 }
-void updateProcesses(struct process proc[], int pTime, int currentProcess){
-	for(int i = currentProcess; i<nProcess; i++){
-		float waitTime = pTime - proc[i].arrivalTime;
-		proc[i].priority = 1  + waitTime/(float)proc[i].burstTime;
-	}
-	for(int i=currentProcess; i<nProcess-1; i++){
-		for(int j=i+1; j<nProcess; j++){
-			if(proc[i].priority < proc[j].priority){
-				struct process a = proc[j];
-				proc[j] = proc[i];
-				proc[i] = a;
-			}
-		}
-	}
-}
+
 
 void printProcess(struct process proc[]){
 	for(int i=0; i<nProcess; i++){
-		printf("The Proccess :%d arivlTime : %d  burstTime : %d\n\n", proc[i].id, proc[i].arrivalTime, proc[i].burstTime);
+		printf("The Proccess :%d arrival time : %d  burst time is: %d\n\n", proc[i].id, proc[i].arrivalTime, proc[i].burstTime);
 	}
 }
 
 int main()
 {
-	printf("Enter the no. of Process\n");
+	printf("Enter the number of Process\n");
 	scanf("%d",&nProcess);
 	while(nProcess < 1){
-		printf("Enter Process Count greater than one.\n");
+		printf("Enter Process count greater than one.\n");
 		scanf("%d",&nProcess);
 	}
 	struct process proc[nProcess];
